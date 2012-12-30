23fec649b50b0aa838e6f226de2cedacfc32292b
Ed Roman <edward.w.roman@gmail.com>
2012-12-30 07:05:04 -0800

    initial commit

---
+++ Improv/ImprovAppDelegate.h
@@ -0,0 +1,11 @@
+@class ImprovViewController;
+
+@interface ImprovAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, strong) IBOutlet UIWindow *window;
+
+@property (nonatomic, strong) IBOutlet ImprovViewController *viewController;
+
+@end

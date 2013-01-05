aeb45adb0304192511b6b84c14e7595363182591
Ed Roman <edward.w.roman@gmail.com>
2013-01-05 10:25:31 -0800

    lobby improvements

--- Improv/ImprovAppDelegate.m
+++ Improv/ImprovAppDelegate.m
@@ -8,93 +8,93 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    ///////////////////////////////////////
-    // Parse Initialization
-    ///////////////////////////////////////
-    
-    [Parse setApplicationId:@"WTbIj7pY3jJC3cnqxF2cidV164TOWxgTtbGfjGnF" clientKey:@"EjV6lQXjI0S35MYcaoPPkhgRXCaYvU1J9B59lvAa"];
+	///////////////////////////////////////
+	// Parse Initialization
+	///////////////////////////////////////
+	
+	[Parse setApplicationId:@"WTbIj7pY3jJC3cnqxF2cidV164TOWxgTtbGfjGnF" clientKey:@"EjV6lQXjI0S35MYcaoPPkhgRXCaYvU1J9B59lvAa"];
 
-    // Setup Facebook App ID.  Also done in Project Settings.
-    [PFFacebookUtils initializeWithApplicationId:@"250634021702621"];
+	// Setup Facebook App ID.  Also done in Project Settings.
+	[PFFacebookUtils initializeWithApplicationId:@"250634021702621"];
 
-    [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|
-                                                    UIRemoteNotificationTypeAlert|
-                                                    UIRemoteNotificationTypeSound];
-    return YES;
+	[application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|
+													UIRemoteNotificationTypeAlert|
+													UIRemoteNotificationTypeSound];
+	return YES;
 }
 
 // For Facebook-Parse integration
 - (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
-    return [PFFacebookUtils handleOpenURL:url];
+	return [PFFacebookUtils handleOpenURL:url];
 }
 
 // For Facebook-Parse integration
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
   sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
-    return [PFFacebookUtils handleOpenURL:url];
+	return [PFFacebookUtils handleOpenURL:url];
 }
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)newDeviceToken {
-    [PFPush storeDeviceToken:newDeviceToken];
-    [PFPush subscribeToChannelInBackground:@"" target:self selector:@selector(subscribeFinished:error:)];
+	[PFPush storeDeviceToken:newDeviceToken];
+	[PFPush subscribeToChannelInBackground:@"" target:self selector:@selector(subscribeFinished:error:)];
 }
 
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-    if (error.code == 3010) {
-        NSLog(@"Push notifications are not supported in the iOS Simulator.");
-    } else {
-        // show some alert or otherwise handle the failure to register.
-        NSLog(@"application:didFailToRegisterForRemoteNotificationsWithError: %@", error);
+	if (error.code == 3010) {
+		NSLog(@"Push notifications are not supported in the iOS Simulator.");
+	} else {
+		// show some alert or otherwise handle the failure to register.
+		NSLog(@"application:didFailToRegisterForRemoteNotificationsWithError: %@", error);
 	}
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    [PFPush handlePush:userInfo];
+	[PFPush handlePush:userInfo];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
+	/*
+	 Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+	 Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+	 */
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
+	/*
+	 Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+	 If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+	 */
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
+	/*
+	 Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+	 */
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
+	/*
+	 Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+	 */
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    /*
-     Called when the application is about to terminate.
-     Save data if appropriate.
-     See also applicationDidEnterBackground:.
-     */
+	/*
+	 Called when the application is about to terminate.
+	 Save data if appropriate.
+	 See also applicationDidEnterBackground:.
+	 */
 }
 
 
 #pragma mark - ()
 
 - (void)subscribeFinished:(NSNumber *)result error:(NSError *)error {
-    if ([result boolValue]) {
-        NSLog(@"Improv successfully subscribed to push notifications on the broadcast channel.");
-    } else {
-        NSLog(@"Improv failed to subscribe to push notifications on the broadcast channel.");
-    }
+	if ([result boolValue]) {
+		NSLog(@"Improv successfully subscribed to push notifications on the broadcast channel.");
+	} else {
+		NSLog(@"Improv failed to subscribe to push notifications on the broadcast channel.");
+	}
 }
 
 
--- Improv/LoginViewController.m
+++ Improv/LoginViewController.m
@@ -10,14 +10,14 @@
 #pragma mark - UIViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    self.title = @"Facebook Profile";
-    
-    // Check if user is cached and linked to Facebook, if so, bypass login    
-    if ([PFUser currentUser] && [PFFacebookUtils isLinkedWithUser:[PFUser currentUser]]) {
-        NSLog(@"User logged in already - bypassing Facebook login");
-        [self performSegueWithIdentifier:@"LoginToLobbySegue" sender:nil];
-    }
+	[super viewDidLoad];
+	self.title = @"Facebook Profile";
+	
+	// Check if user is cached and linked to Facebook, if so, bypass login	
+	if ([PFUser currentUser] && [PFFacebookUtils isLinkedWithUser:[PFUser currentUser]]) {
+		NSLog(@"User logged in already - bypassing Facebook login");
+		[self performSegueWithIdentifier:@"LoginToLobbySegue" sender:nil];
+	}
 }
 
 
@@ -25,38 +25,38 @@
 
 /* Login to facebook method */
 - (IBAction)loginButtonTouchHandler:(id)sender  {
-    // Set permissions required from the facebook user account
-    NSArray *permissionsArray = @[ @"email"];
-    
-    // Login PFUser using facebook
-    [PFFacebookUtils logInWithPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
-        [_activityIndicator stopAnimating]; // Hide loading indicator
-        
-        if (!user) {
-            if (!error) {
-                NSLog(@"Uh oh. The user cancelled the Facebook login.");
-                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Log In Error" message:@"Uh oh. The user cancelled the Facebook login." delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Dismiss", nil];
-                [alert show];
-            } else {
-                NSLog(@"Uh oh. An error occurred: %@", error);
-                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Log In Error" message:[error description] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Dismiss", nil];
-                [alert show];
-            }
-        } else if (user.isNew) {
-            NSLog(@"User with facebook signed up and logged in!");
-            // Create Parse User
-            
-            // Load Lobby
-            [self performSegueWithIdentifier:@"LoginToLobbySegue" sender:sender];
-        } else {
-            NSLog(@"User with facebook logged in!");
-            
-            // Load Lobby
-            [self performSegueWithIdentifier:@"LoginToLobbySegue" sender:sender];
-        }
-    }];
-    
-    [_activityIndicator startAnimating]; // Show loading indicator until login is finished
+	// Set permissions required from the facebook user account
+	NSArray *permissionsArray = @[ @"email"];
+	
+	// Login PFUser using facebook
+	[PFFacebookUtils logInWithPermissions:permissionsArray block:^(PFUser *user, NSError *error) {
+		[_activityIndicator stopAnimating]; // Hide loading indicator
+		
+		if (!user) {
+			if (!error) {
+				NSLog(@"Uh oh. The user cancelled the Facebook login.");
+				UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Log In Error" message:@"Uh oh. The user cancelled the Facebook login." delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Dismiss", nil];
+				[alert show];
+			} else {
+				NSLog(@"Uh oh. An error occurred: %@", error);
+				UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Log In Error" message:[error description] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Dismiss", nil];
+				[alert show];
+			}
+		} else if (user.isNew) {
+			NSLog(@"User with facebook signed up and logged in!");
+			// Create Parse User
+			
+			// Load Lobby
+			[self performSegueWithIdentifier:@"LoginToLobbySegue" sender:sender];
+		} else {
+			NSLog(@"User with facebook logged in!");
+			
+			// Load Lobby
+			[self performSegueWithIdentifier:@"LoginToLobbySegue" sender:sender];
+		}
+	}];
+	
+	[_activityIndicator startAnimating]; // Show loading indicator until login is finished
 }
 
 @end

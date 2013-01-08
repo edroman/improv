7de38de61e258d746eb9e218af29dddf1f1badb8
Ed Roman <edward.w.roman@gmail.com>
2013-01-08 15:27:48 -0800

    added controls to main menu view

--- Improv/LobbyViewController.m
+++ Improv/LobbyViewController.m
@@ -13,22 +13,28 @@
 
 // Child view controller(s)
 @property (nonatomic, strong) UIViewController *tableVC;
+@property (nonatomic, strong) UIViewController *menuVC;
 
 @end
 
 @implementation LobbyViewController
 
 @synthesize tableVC = _tableVC;
+@synthesize menuVC = _menuVC;
 
 -(void)awakeFromNib{
 	// instantiate and assign the child view controller to a property to have direct reference to it in
 	self.tableVC = [self.storyboard instantiateViewControllerWithIdentifier:@"StoryTableViewController"];
+	self.menuVC = [self.storyboard instantiateViewControllerWithIdentifier:@"MainMenuViewController"];
 
 	// ... if necessary, configure your child view controller ...
 	
 	// add your child view controller to children array
+	[self addChildViewController:self.menuVC];
 	[self addChildViewController:self.tableVC];
-	
+
+	// Signal the child content view controller that it has been added to the container view controller
+	[self.menuVC didMoveToParentViewController:self];
 	[self.tableVC didMoveToParentViewController:self];
 }
 
@@ -37,9 +43,11 @@
 	[super viewDidLoad];
 
 	// configure chld view controller view's frame
-	self.tableVC.view.frame=CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height);
+	self.menuVC.view.frame=CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height);
+	self.tableVC.view.frame=CGRectMake(0, 75, self.view.bounds.size.width, self.view.bounds.size.height-75);
 
 	// add child's view to view hierarchy
+	[self.view addSubview:self.menuVC.view];
 	[self.view addSubview:self.tableVC.view];
 
 	// Uncomment the following line to preserve selection between presentations.
---
+++ Improv/MainMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainMenuViewController.m
+//  Improv
+//
+//  Created by Ed Roman on 1/8/13.
+//  Copyright (c) 2013 Ghostfire Games. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

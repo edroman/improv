280acee012834deb9ad058eb168102867fc354ac
Ed Roman <edward.w.roman@gmail.com>
2013-02-12 12:02:42 -0800

    fixed login scrollview

--- Improv/LoginViewController.h
+++ Improv/LoginViewController.h
@@ -2,12 +2,13 @@
 //  Copyright (c) 2012 Parse. All rights reserved.
 
 #import <UIKit/UIKit.h>
+#import "TPKeyboardAvoidingScrollView.h"
 
-@interface LoginViewController : UIViewController<UIScrollViewDelegate, UITextFieldDelegate>
+@interface LoginViewController : UIViewController <UITextFieldDelegate, UIScrollViewDelegate>
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField;
 //- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField;
-- (BOOL)textFieldShouldEndEditing:(UITextField *)textField;
+//- (BOOL)textFieldShouldEndEditing:(UITextField *)textField;
 
 @property (nonatomic, strong) IBOutlet UIActivityIndicatorView *activityIndicator;
 
